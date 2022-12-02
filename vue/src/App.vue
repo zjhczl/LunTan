bd2b3af27d9287e9299225347714ce9d65c99540
unknown <2728774085@qq.com>
2022-12-02 16:22:25 +0800

    添加vue

---
+++ vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  /* overflow: hidden; */
+  /* margin: 0; */
+  height: 100vh;
+  padding: 0.5rem;
+}
+</style>
